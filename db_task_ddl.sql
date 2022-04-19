81fa43b3e361ad276ac52b5036b9ab954023eed3
ueharajuki <22uehara_juki@axiz.co.jp>
2022-04-19 17:54:48 +0900

    単元課題2基礎ER図とDDL完成

---
+++ db_task_ddl.sql
@@ -0,0 +1,62 @@
+create table NEW_ENTITY8 (
+  "“`•[”Ô†" integer
+  , "¤•iID" integer
+  , "ŒÂ”" integer
+  , "x•¥•û–@ID" integer
+  , "¤•i‚²‚Æ‚Ì”„ã" integer
+  , "’lˆøŠz" integer
+  , "Á”ïÅID" integer
+  , constraint NEW_ENTITY8_PKC primary key ("“`•[”Ô†","¤•iID","x•¥•û–@ID","Á”ïÅID")
+) ;
+
+
+create table NEW_ENTITY7 (
+  "ƒŒƒWID" integer
+  , "“`•[”Ô†" integer
+  , "‰ïĞID" integer not null
+  , "]‹ÆˆõID" integer
+  , "”„ã“ú" character varying
+  , constraint NEW_ENTITY7_PKC primary key ("ƒŒƒWID","“`•[”Ô†","‰ïĞID")
+) ;
+
+
+create table NEW_ENTITY6 (
+  "“X•ÜID" integer
+  , "“X•Ü–¼" character varying
+  , constraint NEW_ENTITY6_PKC primary key ("“X•ÜID")
+) ;
+
+
+create table NEW_ENTITY5 (
+  "]‹ÆˆõID" integer
+  , "Ó”CÒNo" integer
+  , constraint NEW_ENTITY5_PKC primary key ("]‹ÆˆõID")
+) ;
+
+create table NEW_ENTITY4 (
+  "x•¥•û–@ID" integer
+  , "x•¥•û–@" character varying
+  , constraint NEW_ENTITY4_PKC primary key ("x•¥•û–@ID")
+) ;
+
+
+create table NEW_ENTITY3 (
+  "‰ïĞID" integer
+  , "‰ïĞ–¼" character varying
+  , constraint NEW_ENTITY3_PKC primary key ("‰ïĞID")
+) ;
+
+
+create table NEW_ENTITY2 (
+  "Á”ïÅID" integer
+  , "Á”ïÅ—¦" integer
+  , constraint NEW_ENTITY2_PKC primary key ("Á”ïÅID")
+) ;
+
+
+create table NEW_ENTITY1 (
+  "¤•iID" integer
+  , "¤•i–¼" character varying
+  , "¤•i’P‰¿" 
+  , constraint NEW_ENTITY1_PKC primary key ("¤•iID")
+) ;
